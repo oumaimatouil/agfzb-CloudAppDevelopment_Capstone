10f25a22f975bdc71796e6972abcdefce48ed0d6
oumaimatouil <oumaima.touil98@gmail.com>
2023-09-24 06:10:25 -0400

    some changements in backend services
    Signed-off-by: oumaimatouil <oumaima.touil98@gmail.com>

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -40,6 +40,17 @@ def get_request(url,api_key=False, **kwargs):
         print("Failed to fetch data. Status code:", status_code)
         return None
 
+# Function for making HTTP POST requests
+def post_request(url, json_payload, **kwargs):
+    print(f"POST to {url}")
+    try:
+        response = requests.post(url, params=kwargs, json=json_payload)
+    except:
+        print("An error occurred while making POST request. ")
+    status_code = response.status_code
+    print(f"With status {status_code}")
+
+    return response 
 # Create a function `get_dealers_from_cf` to get dealers from a cloud function
 def get_dealers_from_cf(url, **kwargs):
     results = []
@@ -169,45 +180,38 @@ def get_dealer_reviews_from_cf(url, dealer_id, **kwargs):
     
     return results
 # Calls the Watson NLU API and analyses the sentiment of a review
+
+import json
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson.natural_language_understanding_v1 import Features, SentimentOptions
+
 def analyze_review_sentiments(dealerreview):
     # Define your IBM Watson NLU API key and service URL
-    ibm_api_key = 'api_key'
-    ibm_service_url = 'service_url'
+    ibm_api_key = 'Zz6gG6Cnxeas-YNOqFvDuznFt1UU-IYcnZyO8ywowoRI'
+    ibm_service_url = 'https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/d3f3cb64-9a75-4cc2-9426-e4679e8d8981'
 
-    # Initialize the IBM Watson NLU client
-    authenticator = IAMAuthenticator(ibm_api_key)
-    natural_language_understanding = NaturalLanguageUnderstandingV1(
-        version='2022-04-07',
-        authenticator=authenticator
-    )
-    natural_language_understanding.set_service_url(ibm_service_url)
-
-    # Define the text to analyze (assuming 'dealerreview' contains the text)
-    text_to_analyze = dealerreview
-
-    # Define the NLU features you want to extract
-    nlu_features = Features(
-        entities=EntitiesOptions(emotion=True, sentiment=True, limit=2),
-        keywords=KeywordsOptions(emotion=True, sentiment=True, limit=2)
-    )
-
-    # Create a dictionary with the parameters for the get_request function
-    params = {
-        "text": text_to_analyze,
-        "version": '2022-04-07',
-        "features": nlu_features,
-        "return_analyzed_text": True  # Adjust this as needed
-    }
-
-    # Define the URL for your get_request function
-    nlu_url = 'service_url'  # Replace with the actual URL
-
-    # Make a call to the get_request function
     try:
-        nlu_response = get_request(nlu_url, api_key=ibm_api_key, **params)
-        print("hi from here")
-        print(nlu_response)
-        return nlu_response
+        # Initialize the IBM Watson NLU client
+        authenticator = IAMAuthenticator(ibm_api_key)
+        natural_language_understanding = NaturalLanguageUnderstandingV1(
+            version='2022-04-07',
+            authenticator=authenticator
+        )
+        natural_language_understanding.set_service_url(ibm_service_url)
+
+        # Analyze sentiment using the SDK
+        response = natural_language_understanding.analyze(
+            text=dealerreview,
+            features=Features(sentiment=SentimentOptions())
+        ).get_result()
+
+        return response
+
     except Exception as e:
         print('An error occurred while analyzing sentiments:', str(e))
         return None
+
+# Example usage
+sentiment = analyze_review_sentiments("Optional heuristic software")
+print(json.dumps(sentiment, indent=2))
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -121,9 +121,13 @@ def get_dealer_details(request, dealer_id):
          # Concat all dealer's short name
         reviews_list= [review.review for review in reviews]
         dealerships_list= [review.dealership for review in reviews]
+        sentiment_list = [review.sentiment for review in reviews]
         result = [(i,j) for i,j in zip(dealerships_list, reviews_list)]
+        result_sentiment = [(i,j) for i,j in zip(result, sentiment_list)]
 
-        return HttpResponse(result)
+
+        return HttpResponse(result_sentiment)
+
 
 
 
@@ -133,6 +137,44 @@ def get_dealer_details(request, dealer_id):
 # ...
 
 # Create a `add_review` view to submit a review
-# def add_review(request, dealer_id):
-# ...
+# View to submit a new review
+def add_review(request, dealer_id):
+    # User must be logged in before posting a review
+    if request.user.is_authenticated:
 
+        # POST request posts the content in the review submission form to the Cloudant DB using the post_review Cloud Function
+        if request.method == "POST":
+            form = request.POST
+            review = dict()
+            review["name"] = f"{request.user.first_name} {request.user.last_name}"
+            review["dealership"] = dealer_id
+            review["review"] = form["content"]
+            review["purchase"] = form.get("purchasecheck")
+            if review["purchase"]:
+                review["purchase_date"] = datetime.strptime(form.get("purchasedate"), "%m/%d/%Y").isoformat()
+            car = CarModel.objects.get(pk=form["car"])
+            review["car_make"] = car.car_make.name
+            review["car_model"] = car.name
+            review["car_year"] = car.year
+            
+            # If the user bought the car, get the purchase date
+            if form.get("purchasecheck"):
+                review["purchase_date"] = datetime.strptime(form.get("purchasedate"), "%m/%d/%Y").isoformat()
+            else: 
+                review["purchase_date"] = None
+
+            url = "https://oumaimatouil-5000.theiadocker-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/api/get_reviews"  # API Cloud Function route
+            json_payload = {"review": review}  # Create a JSON payload that contains the review data
+
+            # Performing a POST request with the review
+            result = post_request(url, json_payload, dealerId=dealer_id)
+            if int(result.status_code) == 200:
+                print("Review posted successfully.")
+
+            # After posting the review the user is redirected back to the dealer details page
+            return redirect("djangoapp:dealer_details", dealer_id=dealer_id)
+
+    else:
+        # If user isn't logged in, redirect to login page
+        print("User must be authenticated before posting a review. Please log in.")
+        return redirect("/djangoapp/login")
